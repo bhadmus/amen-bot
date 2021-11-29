47cfb9ea032ad9c5addc1c2334a3b61a28af0574
Ademola Bhadmus <bhadmusademola.1@gmail.com>
2021-11-29 06:56:13 +0000

    used fixtures feature

--- cypress/integration/test-classes/first.spec.js
+++ cypress/integration/test-classes/first.spec.js
@@ -1,11 +1,38 @@
 context('Creating a demo class', () =>{
-    it('It should open the demo site', () =>{
-        cy.visit('/');
+it('It should open the demo site', () =>{
+    cy.visit('/');
+})
+it('It should close the banner', () =>{
+    cy.fixture('element-mappers').then((em) =>{
+        cy.get(em.banner).click();
+
     })
-    it('It should close the banner', () =>{
-        cy.get('#at-cv-lightbox-close').click();
+})
+it('It should Open the Input Forms dropdown', () =>{
+    cy.fixture('element-mappers').then((em) =>{
+        cy.get(em.inputFormDropDown).click()
+
     })
-    it('It should Open the Input Forms dropdown', () =>{
-        cy.get('#navbar-brand-centered .navbar-nav:nth-of-type(1) .dropdown:nth-of-type(1) [data-toggle]').click()
+})
+
+it('It should select JQuery Options', () =>{
+    cy.fixture('element-mappers').then((em) =>{
+        cy.get(em.jQueryOption).should('be.visible').click();
+
     })
+})
+
+it('It should scroll to the bottom of the page', () =>{
+    cy.fixture('element-mappers').then((em) =>{
+        cy.scrollTo(em.pageDown);
+
+    })
+})
+
+it('It should select Java', () =>{
+    cy.fixture('element-mappers').then((em) =>{
+        cy.get(em.languageDropdown).select(em.java);
+
+    })
+})
 })
